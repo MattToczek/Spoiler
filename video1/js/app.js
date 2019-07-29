b3f94da344e58d1fd7868192bd3328c127054168
Matt. Toczek <toczek.matt@gmail.com>
2019-07-29 15:26:56 +0100

    Adding Spoiler Reveal func - jQuery

--- video1/js/app.js
+++ video1/js/app.js
@@ -1,5 +1,11 @@
+// Hide spoiler on page load
+$('.spoiler span').hide();
 
+//when button= pressed, show spoiler & hide button
+$('.spoiler button').click(function(){
+  let button = $('.spoiler button');
+  let spoilerText = $('.spoiler span');
 
-
-
-
+  button.hide();
+  spoilerText.show();
+});
